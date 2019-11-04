dd79af87d5e3828391e64691aeaf87be257b02b8
ThAnOs <a.lico@vibre.io>
2019-11-04 16:33:07 +0100

    release work

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var spotify = require('./spotifyController')
 
-export default function(client_id, client_secret, redirect_uri) {
+module.exports = function(client_id, client_secret, redirect_uri) {
     return new spotify(client_id, redirect_uri, client_secret)
 }
